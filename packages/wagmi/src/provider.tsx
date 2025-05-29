962563dc82071063280b483e001d79e7f40f1e2d
plaxswap <plaxswap@gmail.com>
2025-05-29 23:00:33 +0800

    update

--- packages/wagmi/src/provider.tsx
+++ packages/wagmi/src/provider.tsx
@@ -1,8 +1,18 @@
-
 import { Web3Provider } from '@ethersproject/providers'
 import React from 'react'
 import useSWRImmutable from 'swr/immutable'
-import { useAccount, useNetwork, WagmiConfig } from 'wagmi'
+import { useAccount, WagmiConfig, WagmiConfigProps, useNetwork } from 'wagmi'
+import { Provider, WebSocketProvider } from '@wagmi/core'
+
+export function WagmiProvider<TProvider extends Provider, TWebSocketProvider extends WebSocketProvider>(
+  props: React.PropsWithChildren<WagmiConfigProps<TProvider, TWebSocketProvider>>,
+) {
+  return (
+    <WagmiConfig client={props.client}>
+      <Web3LibraryProvider>{props.children}</Web3LibraryProvider>
+    </WagmiConfig>
+  )
+}
 
 const Web3LibraryContext = React.createContext<Web3Provider | undefined>(undefined)
 
@@ -13,28 +23,10 @@ export const useWeb3LibraryContext = () => {
 const Web3LibraryProvider: React.FC<React.PropsWithChildren> = (props) => {
   const { connector } = useAccount()
   const { chain } = useNetwork()
+  const { data: library } = useSWRImmutable(connector && ['web3-library', connector, chain], async () => {
+    const provider = await connector?.getProvider()
+    return new Web3Provider(provider)
+  })
 
-  const { data: library } = useSWRImmutable(
-    connector && ['web3-library', connector, chain],
-    async () => {
-      const provider = await connector?.getProvider()
-      return new Web3Provider(provider)
-    }
-  )
-
-  return (
-    <Web3LibraryContext.Provider value={library}>
-      {props.children}
-    </Web3LibraryContext.Provider>
-  )
-}
-
-export function WagmiProvider(
-  props: React.PropsWithChildren<{ client: ReturnType<typeof import('wagmi').createConfig> }>
-) {
-  return (
-    <WagmiConfig config={props.client}>
-      <Web3LibraryProvider>{props.children}</Web3LibraryProvider>
-    </WagmiConfig>
-  )
+  return <Web3LibraryContext.Provider value={library}>{props.children}</Web3LibraryContext.Provider>
 }
